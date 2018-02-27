d23b2f5e7da6815f73cc2d77c927f2ce8e29ab94
zhibli <zhibli@redhat.com>
2018-02-27 16:18:02 +0800

    get the latest(variable) version of hadoop and some other stuff
    use wget, curl, sed, grep to get the current version of hadoop
    and to extract it to the pointed path with the owner being current user.

---
+++ get-latest-version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Author: Zhibin Li <08826794brmt@gmail.com>
+
+
+#VARIABLES
+default_path=/usr/local/hadoop
+current_user=`printenv | sed -n 's/SUDO_USER=\(.*\)/\1/p'`
+
+echo -n "Enter directory in which to extract the package (/usr/local/hadoop): "
+read hadoop_path
+HADOOP_HOME=${hadoop_path:-$default_path}
+
+#download the latest version of hadoop
+wget http://mirror.bit.edu.cn/apache/hadoop/common/current/`curl -s http://mirror.bit.edu.cn/apache/hadoop/common/current/|sed -n 's/.*\(hadoop-.*.tar.gz\).*/\1/p'|grep -v src` -O hadoop.tar.gz
+
+#extract to HADOOP_HOME
+rm -rf $HADOOP_HOME
+mkdir $HADOOP_HOME
+tar -zxf hadoop.tar.gz -C $HADOOP_HOME --strip-components=1
+rm -rf hadoop.tar.gz
+chown -R $current_user:$current_user $HADOOP_HOME
