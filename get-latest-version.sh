94e3015e4d429ac293720105e9d44783ddfcac5f
Zhibin Li <08826794brmt@gmail.com>
2018-03-25 00:58:18 +0800

    add is_root detect and format adjustment
    Signed-off-by: Zhibin Li <08826794brmt@gmail.com>

--- get-latest-version.sh
+++ get-latest-version.sh
@@ -7,12 +7,20 @@
 default_path=/usr/local/hadoop
 current_user=`printenv | sed -n 's/SUDO_USER=\(.*\)/\1/p'`
 
+if [ $UID -ne 0 ]; then
+	echo 'need to run as root'
+	exit 1
+fi
+
 echo -n "Enter directory in which to extract the package (/usr/local/hadoop): "
 read hadoop_path
 HADOOP_HOME=${hadoop_path:-$default_path}
 
 #download the latest version of hadoop
-wget http://mirror.bit.edu.cn/apache/hadoop/common/current/`curl -s http://mirror.bit.edu.cn/apache/hadoop/common/current/|sed -n 's/.*\(hadoop-.*.tar.gz\).*/\1/p'|grep -v src` -O hadoop.tar.gz
+wget http://mirror.bit.edu.cn/apache/hadoop/common/current/\
+`curl -s http://mirror.bit.edu.cn/apache/hadoop/common/current/| \
+sed -n 's/.*\(hadoop-.*.tar.gz\).*/\1/p'| \
+grep -v src` -O hadoop.tar.gz
 
 #extract to HADOOP_HOME
 rm -rf $HADOOP_HOME
